869b25539948936f12af45d90700389eccc525a2
deepanshu-mahawar <deepanshu.mahawar@kriotek.in>
2025-12-01 13:46:18 +0530

    add fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Cart from "./features/Cart";
 import { Counter } from "./features/Counter";
 import { FoodList } from "./features/FoodList";
 import { LightBulb } from "./features/LightBulb";
 import { UserList } from "./features/userList";
+import UserData from "./features/UserData";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchUsers = async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data = await res.json();
-      setUsers(data);
-    } catch (error) {
-      console.log("Error fetching users:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  if (loading) return <h2>Loading...</h2>;
-
   return (
     <div>
       <h1>Vite + React + Redux Toolkit</h1>
@@ -36,14 +15,7 @@ function App() {
       {/* <FoodList/> */}
       {/* <Cart/> */}
       {/* <UserList /> */}
-      <h2>User List</h2>
-      <ul>
-        {users.map((u) => (
-          <li key={u.id}>
-            <strong>{u.name}</strong> — {u.email}
-          </li>
-        ))}
-      </ul>
+      <UserData />
     </div>
   );
 }
---
+++ src/features/UserData.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export default function UserData() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      console.log("Error fetching users:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>
+            <strong>{u.name}</strong> — {u.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
